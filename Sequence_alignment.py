582718b6556bb972bc5c06f874841e23c97e2a60
rdk004 <109682061+rdk004@users.noreply.github.com>
2023-08-13 11:48:09 +0530

    Sequence_alignment.py

--- Sequence_alignment.py
+++ Sequence_alignment.py
@@ -3,13 +3,13 @@ import itertools
 
 class Seq_alignment(): 
     '''
-    This class contains a total of 7 methods and an initializing constructor.
+    This class contains a total of 7 methods including an initializing constructor.
     Let us see what each function is doing in this program:
         1) __init__ - This initializes the variables seq_1,seq_2,l1,l2 through the class object "construct_sa".
         2) permutation_tuple_duplicates - This creates two two-dimensional arrays of the different permutations of the respective nucleic acid strings (including the gaps).
         3) remove_duplicates - This function removes the duplicates in the two arrays created in the previous function.
-        4) twod_to_oned - This function converts the two dimensional arrays into one dimensional arrays for further processing.
-        5) sequence_array - This function removes the "_" character from each string of the two one dimensional arrays to compare the resultant strings with the two respective original sequences for futher elimination.
+        4) twod_to_oned - This function converts the two-dimensional arrays into one-dimensional arrays for further processing.
+        5) sequence_array - This function removes the "_" character from each string of the two one-dimensional arrays to compare the resultant strings with the two respective original sequences for futher elimination.
         6) deletion - This function removes the strings from the two one-dimensional arrays whose sequences (without the gaps: "_"), don't match the original two sequences. 
         7) oned_to_twod - This function creates a new one-dimensional array which contains all possible combinations of strings from both the first and second string. Next, it converts this one-dimensional array into a two-dimensional array with each sub-array of length 2 to compare each permutation of the two sequences (with/without gaps).
         8) underscore_comp - This function compares the two strings in each sub-array of the two-dimensional array character by character to check if a gap doesn't align with another gap because it doesn't make sense.
@@ -136,9 +136,55 @@ class Seq_alignment():
         for j in final_del_arr:
             final_seq_arr.remove(j)
          
-        return (final_seq_arr,len(final_seq_arr))    
+        return (final_seq_arr,len(final_seq_arr)) 
+    
+    def alignment_score(final_array):
+        # We will consider an transition-transversion scoring matrix in this case
+        # {"A-T":-7,"A-G":-5,"A-C":-7,"T-G":-7,"T-C":-5,"G-C":-7,"A-A":2,"T-T":2,"G-G":2,"C-C":2}
+        opening_gap_pen = 10
+        continuous_gap_pen = 0.5
+        alignment_score = 0
+        length_final = len(final_array)
+        alignment_score_array=[]
+        for i in range(0,length_final):
+            a = final_array[i][0]
+            b = final_array[i][1]
+            u_length = len(a)
+            for j in range(0,u_length):
+                if j==0:
+                    if a[j] == "_" or b[j] == "_":
+                        alignment_score-=opening_gap_pen
+                    elif (a[j]=="A" and b[j]=="G") or (a[j]=="G" and b[j]=="A") or (a[j]=="T" and b[j]=="C") or (a[j]=="C" and b[j]=="T"):
+                         alignment_score-=5
+                    elif (a[j]=="A" and b[j]=="T") or (a[j]=="C" and b[j]=="G") or (a[j]=="T" and b[j]=="A") or (a[j]=="G" and b[j]=="C") or (a[j]=="A" and b[j]=="C") or (a[j]=="C" and b[j]=="A") or (a[j]=="G" and b[j]=="T") or (a[j]=="T" and b[j]=="G"):
+                         alignment_score-=7
+                    elif (a[j]=="A" and b[j]=="A") or (a[j]=="C" and b[j]=="C") or (a[j]=="T" and b[j]=="T") or (a[j]=="G" and b[j]=="G"):
+                         alignment_score+=2
+                else:
+                    if (a[j-1]!="_" and a[j]=="_") or (b[j-1]!="_" and b[j]=="_") :
+                        alignment_score-=opening_gap_pen
+                    elif (a[j-1]=="_" and a[j]=="_") or (b[j-1]=="_" and b[j]=="_"):
+                        alignment_score-=continuous_gap_pen
+                    elif (a[j]=="A" and b[j]=="G") or (a[j]=="G" and b[j]=="A") or (a[j]=="T" and b[j]=="C") or (a[j]=="C" and b[j]=="T"):
+                        alignment_score-=5
+                    elif (a[j]=="A" and b[j]=="T") or (a[j]=="C" and b[j]=="G") or (a[j]=="T" and b[j]=="A") or (a[j]=="G" and b[j]=="C") or (a[j]=="A" and b[j]=="C") or (a[j]=="C" and b[j]=="A") or (a[j]=="G" and b[j]=="T") or (a[j]=="T" and b[j]=="G"):
+                        alignment_score-=7
+                    elif (a[j]=="A" and b[j]=="A") or (a[j]=="C" and b[j]=="C") or (a[j]=="T" and b[j]=="T") or (a[j]=="G" and b[j]=="G"):
+                        alignment_score+=2
+                             
+            alignment_score_array.append(alignment_score)
+            alignment_score=0
+        
+        max_alignment_score = max(alignment_score_array)  
+        optimal_alignment_index = alignment_score_array.index(max_alignment_score)
+        optimal_alignment = final_array[optimal_alignment_index]
+        return (max_alignment_score, optimal_alignment)
+                            
+                                
                 
-print("Please enter two sequences of nucleic acid (either DNA or RNA): ") #Assume l2 is greater than l1
+        
+              
+print("Please enter two sequences of DNA: ") #Assume l2 is greater than l1
 seq_1 = input()
 seq_2 = input()
 l1 = len(seq_1)
@@ -172,7 +218,18 @@ final_seq_2 = string_array_temp_tuple[1]
 string_array_temp_2d_value = Seq_alignment.oned_to_twod(final_seq_1,final_seq_2)
 
 final_arr_and_length = Seq_alignment.underscore_comp(string_array_temp_2d_value)
+
+alignment_score_tup = Seq_alignment.alignment_score(final_arr_and_length[0])
+print("\n\n")
 print("These are all the possbile alignments: ")
 print(final_arr_and_length[0])
-print("\n\n")
+print("\n")
 print("The total number of alignments possible for these sequences are :",final_arr_and_length[1])
+print("\n")
+print("Here are some of the parameters")
+print("The gap opening penalty is: -10")
+print("The gap extension penalty is: -0.5")
+print("The substitution matrix used is: \"Transition-Transversion\"")
+print("\n")
+print("The optimal alignment is: ",alignment_score_tup[1])
+print("The optimal alignment score is: ",alignment_score_tup[0])
